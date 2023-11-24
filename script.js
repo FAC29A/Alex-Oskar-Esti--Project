139236b72fbc124bfba30faa7fd6c50896c80c80
Alexander <alex@alexvoiceover.com>
2023-11-24 17:28:35 +0000

    Added .env to gitignore

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+require("dotenv/config");
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("message", (message) => {
+  if (message.author.bot) return; // Ignore messages from bots
+
+  // Process the message here
+  console.log(`Message from ${message.author.tag}: ${message.content}`);
+});
